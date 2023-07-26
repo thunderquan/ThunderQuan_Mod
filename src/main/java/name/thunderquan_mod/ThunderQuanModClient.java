9e2619746d71d71ad65ce8f340b88b51717a19c8
thunderquan <3160649963@qq.com>
2023-07-26 17:00:51 +0800

    Initial commit

---
+++ src/main/java/name/thunderquan_mod/ThunderQuanModClient.java
@@ -0,0 +1,10 @@
+package name.thunderquan_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThunderQuanModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
